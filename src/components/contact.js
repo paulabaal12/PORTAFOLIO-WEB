a2fa5efbb0bd343fb013f6108f1cc6f9023bf5c9
paulabaal12 <106341373+paulabaal12@users.noreply.github.com>
2024-05-21 15:51:00 -0600

    finish contact section

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,7 +1,27 @@
+import styles from '../styles/Contact.module.css';
+
 export default function Contact() {
-    return (
-      <section id="contact">
-           <h2>Contacto</h2>
-      </section>
-    )
-  }
+  return (
+    <section id="contact" className={styles.container}>
+      <h2 className={styles.heading}>Contáctame</h2>
+      <div className={styles.iconContainer}>
+        <a
+          href="mailto:bar22764@uvg.gt"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.iconLink}
+        >
+          <img src="/correo.svg" alt="Correo electrónico" className={styles.icon} />
+        </a>
+        <a
+          href="https://github.com/paulabaal12"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.iconLink}
+        >
+          <img src="/github.svg" alt="GitHub" className={styles.icon} />
+        </a>
+      </div>
+    </section>
+  );
+}
